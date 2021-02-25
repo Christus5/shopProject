ba567217dce6ff62b57b6a3ba39afbf193faf6da
tavkhelidzeluka <Tavkhelidzeluka@gmail.com>
2021-02-26 00:03:09 +0400

    update views to generic views

--- user/views.py
+++ user/views.py
@@ -7,66 +7,74 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic import View
+from django.views.generic import View, FormView, CreateView, RedirectView
 
 from ecommerce.models import Cart
 from user.forms import *
 
 
-class LoginView(View):
+class LoginView(FormView):
     template_name = 'user/user_login.html'
+    form_class = AuthenticationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return redirect(self.success_url)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        context['login_form'] = context.get('form')
+        return context
 
     def get(self, request, *args, **kwargs) -> 'HttpResponse':
         if request.user.is_authenticated:
             return redirect(settings.LOGIN_REDIRECT_URL)
-        return render(request, self.template_name, {
-            'login_form': AuthenticationForm()
-        })
+
+        return super().get(request)
 
     def post(self, request, *args, **kwargs) -> 'HttpResponse':
         if request.user.is_authenticated:
             return redirect(settings.LOGIN_REDIRECT_URL)
-        login_form = AuthenticationForm(request=request, data=request.POST)
 
-        if login_form.is_valid():
-            login(request, login_form.get_user())
-            return redirect(settings.LOGIN_REDIRECT_URL)
-
-        return render(request, self.template_name, {
-            'login_form': login_form
-        })
+        return super().post(request)
 
 
 @method_decorator(login_required, name='dispatch')
-class LogoutView(View):
+class LogoutView(RedirectView):
+    permanent = True
+    pattern_name = 'user:login'
+
     def get(self, request, *args, **kwargs) -> 'HttpResponse':
         logout(request)
-        return redirect(settings.LOGOUT_REDIRECT_URL)
+        return super().get(request)
 
 
-class RegisterView(View):
+class RegisterView(CreateView):
     template_name = 'user/user_registration.html'
+    form_class = RegistrationForm
+    success_url = settings.LOGOUT_REDIRECT_URL
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['registration_form'] = context.get('form')
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        Cart(user=User.objects.get(username=form.save(commit=False))).save()
+        messages.success(self.request, f"""Successfully registered {form.cleaned_data['username']}""")
+        return redirect(self.success_url)
 
     def get(self, request, *args, **kwargs) -> 'HttpResponse':
         if request.user.is_authenticated:
             return redirect(settings.LOGIN_REDIRECT_URL)
 
-        registration_form = RegistrationForm()
-        return render(request, self.template_name, {
-            'registration_form': registration_form
-        })
+        return super().get(request)
 
     def post(self, request, *args, **kwargs) -> 'HttpResponse':
         if request.user.is_authenticated:
             return redirect(settings.LOGIN_REDIRECT_URL)
-        registration_form = RegistrationForm(request.POST)
 
-        if registration_form.is_valid():
-            registration_form.save()
-            Cart(user=User.objects.get(username=registration_form.save(commit=False))).save()
-            messages.success(request, f"""Successfully registered {registration_form.cleaned_data['username']}""")
-            return redirect(settings.LOGOUT_REDIRECT_URL)
-
-        return render(request, self.template_name, {
-            'registration_form': registration_form
-        })
+        return super().post(request)
